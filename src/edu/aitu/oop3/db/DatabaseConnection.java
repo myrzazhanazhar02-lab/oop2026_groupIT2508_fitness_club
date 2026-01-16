2b29e481bc2cfde4b3bb89eae7cf5905508c8d11
myrzazhanazhar02-lab <myrzazhanazhar02@gmail.com>
2026-01-16 16:04:49 +0500

    Myrzazhan Azhar добавлена структура папок

---
+++ src/edu/aitu/oop3/db/DatabaseConnection.java
@@ -0,0 +1,19 @@
+package edu.aitu.oop3.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL =
+            "jdbc:postgresql://aws-1-ap-northeast-2.pooler.supabase.com:5432/postgres?sslmode=require";
+    private static final String USER = "postgres.wwlkpayichwwogvjsaqb";
+    private static final String PASSWORD =
+             "Myrzazhan_12";// ← DATABASE PASSWORD
+    private DatabaseConnection() {
+// no instances
+    }
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
